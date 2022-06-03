Ancient-Lab/vendor_ancient

vendor: Add back livedisplay permissions
Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

config/common.mk
@@ -58,6 +58,10 @@ PRODUCT_COPY_FILES += \
     vendor/ancient/config/permissions/privapp-permissions-product-google-ancient.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-google-ancient.xml \
     vendor/ancient/config/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
+# Live Display
+PRODUCT_COPY_FILES += \
+    vendor/ancient/config/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml
+
 # Copy all ancient-OS-specific init rc files
 $(foreach f,$(wildcard vendor/ancient/prebuilt/common/etc/init/*.rc),\
     $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))

Xtended-Q/vendor_xtended

Signed-off-by: rayrzy <vincentrayananda@gmail.com>
Change-Id: Ib7e0578c018cae40181fa96fce7c8969ca506640

config/common.mk
@@ -122,6 +122,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/xtended/config/permissions/xtended-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/xtended-power-whitelist.xml
 
+# Live Display
+PRODUCT_COPY_FILES += \
+    vendor/xtended/config/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml
+
 # Include AOSP audio files
 include vendor/xtended/config/aosp_audio.mk
 